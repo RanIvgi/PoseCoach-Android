46f5bba4a93a6458d724292203480ef734f4210a
noyseg <n3segal@gmail.com>
2025-11-23 06:00:53 +0200

    Dependency added: A new dependency on androidx.navigation:navigation-compose:2.7.5 has been added to enable navigation between screens. Added logos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
     
     // Compose Debug
     debugImplementation("androidx.compose.ui:ui-tooling")
